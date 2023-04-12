49705c57083b45ea4a290cecd0f82538eb92aa0f
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-04-12 21:08:41 +0530

    DOM Manipulation

---
+++ SelectionElementInDom/Script.js
@@ -0,0 +1,30 @@
+//  DOM Manipulation
+
+
+//  GetElementById()
+
+const heading = document.getElementById('Head');
+console.log(heading);
+
+
+// GetElementByClassName()
+
+const temp = document.getElementsByClassName('list-items');
+console.log(temp)
+
+
+// GetElementByTagName()
+
+const listItem = document.getElementsByTagName("li")
+console.log(listItem);
+
+
+// querySelector()
+
+const container = document.querySelector('h1')
+console.log(container)
+
+//  querySelectorAll()
+
+const allSelect = document.querySelectorAll("div");
+console.log(allSelect)

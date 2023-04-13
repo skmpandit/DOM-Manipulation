4a6929e81b6ec60f1fb3cbb63007cdcd80f0b69e
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-04-13 11:44:58 +0530

    Take input from user and change the Heading

---
+++ TakeInputToChangeHeading/Script.js
@@ -0,0 +1,7 @@
+
+
+function btn1() {
+    const select = document.getElementById('Head');
+    const HeadValue = document.getElementById("HeadingInput");
+    select.textContent = HeadValue.value;
+}

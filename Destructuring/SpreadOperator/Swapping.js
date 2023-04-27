e4c824cc50da2d464996d178c14b27951539fb5b
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-04-28 00:07:50 +0530

    Swaping using destructuring

---
+++ Destructuring/SpreadOperator/Swapping.js
@@ -0,0 +1,13 @@
+
+
+//      In this example, two variable are swapped using 
+//      Descturing assignment syntax
+
+let name1 = "Sonu";
+let name2 = "Rajan";
+
+// swapping variable
+[name1, name2] = [name2, name1]
+
+console.log(name1);
+console.log(name2)

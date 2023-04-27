febc19c2315588784f039cbfc4d54a21d49b9c4d
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-04-27 17:39:13 +0530

    Destructuring old way

---
+++ Destructuring/Script.js
@@ -0,0 +1,14 @@
+
+
+            // Accessing the objects the old way 
+
+let person = {
+    firstName: 'Sonu',
+    lastName: 'kumar',
+}
+
+let personFirstName = person.firstName;
+let personLastName = person.lastName;
+
+console.log(personFirstName);
+console.log(personLastName);

f9c9e3f442a6dc9d9c69922442adcb0084832f4a
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-04-27 19:18:13 +0530

    Destructuring the property

--- Destructuring/DestructuringOnePropertyNotExist/Script.js
+++ Destructuring/DestructuringOnePropertyNotExist/Script.js
@@ -1,9 +1,24 @@
 
-const person = {
-    firstName : "Sonu",
-    lastName : "Kumar",
-}
+// const person = {
+//     firstName : "Sonu",
+//     lastName : "Kumar",
+// }
 
             //  what happen if you destruct a property that does not exist
-const { firstName, middleName, lastName } = person;
-console.log(middleName);
+// const { firstName, middleName, lastName } = person;
+// console.log(middleName);
+
+
+            //  Setting defaults when object destructuring
+
+const person = {
+    firstName: 'Sonu',
+    lastName: 'Mahto',
+    middleName: "Kumar",
+    currentAge: 24,
+}
+
+const { firstName, lastName, middleName = '', currentAge: age = 20 } = person;
+console.log(middleName);        // middleName doesn't present in person object but we destruct as a empty but if pesent in person object it print the middlename
+console.log(age);   // the currentAge has already value 24 but we rename as a age and initialize 20 its print 
+                    // 24 because the value is arleady initialize if the value is not initialized than its print 20

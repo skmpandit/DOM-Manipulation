7f70b4a26e45c898197cee633dd500897fc65d6f
Sonu Kumar Mahto <sk3001057@gmail.com>
2023-04-28 17:03:36 +0530

    Function

---
+++ Function/FunctionAreObject.js
@@ -0,0 +1,16 @@
+
+function myFunction (a, b, c) {
+    return arguments.length;
+}
+
+const x = myFunction(2,4,5);
+
+console.log(x);
+
+
+function theFunc (a, b) {
+    return a + b;
+}
+
+const z = theFunc.toString();
+console.log(z)
